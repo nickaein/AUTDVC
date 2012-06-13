3f470f0f39f28807bd49bc326d34cc88c62f49ff
Isaac Nickaein <nickaein.i@gmail.com>
2012-06-13 12:20:55 +0430

    Change directory structure 2

--- AUTDVC/LDPCA.h
+++
@@ -1,5 +0,0 @@
-
-void LDPCAencodeBits(const char *InitByLadderFile, double *source, double *accumulatedSyndrome);
-
-void LDPCAdecodeBits(const char* InitByLadderFile, double *LLR_intrinsic, double *accumulatedSyndrome, double *source,
-                double *decoded, double *rate, double *numErrors);
